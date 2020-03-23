ee54626a7e5039b215c2fc9b75667a1771a92489
shenyu <shenyu@shenyu.me>
2020-03-23 13:36:14 +0800

    Update fetch-shared-apple-id.sh

--- fetch-shared-apple-id.sh
+++ fetch-shared-apple-id.sh
@@ -14,10 +14,15 @@ urlencode() {
 }
 
 url="https://raw.githubusercontent.com/v2net/Apple/master/README.md"
-curl -fsSL $url | grep "免费分享账号" | awk -F '[： ]' '{print $5"\t"$NF}'
+curl -fsSL $url | grep "免费分享账号" | awk -F '[： ]' '{print $3"\t"$5}'
 
 # https://raw.githubusercontent.com/wiki/shadowrocketHelp/help/国外-appstore-id-账号分享.md"
 url="https://raw.githubusercontent.com/wiki/shadowrocketHelp/help/$(urlencode '国外-appstore-id-账号分享.md')"
 url_html=$(curl -fsSL $url)
-echo $url_html | grep -Eo "美区 apple id 账号：\`.+?\`" | awk -F '`' '{printf $2"\t"}'
-echo $url_html | grep -Eo "美区 apple id 密码：\`.+?\`" | awk -F '`' '{print $2}'
+echo $url_html | grep -Eo "【2】美区 apple id 账号：\`.+?\`" | awk -F '`' '{printf $2"\t"}'
+echo $url_html | grep -Eo "【2】美区 apple id 密码：\`.+?\`" | awk -F '`' '{print $2}'
+
+# https://raw.githubusercontent.com/wiki/91CL/91CL-VPN/美区AppleID免费共享.md
+url="https://raw.githubusercontent.com/wiki/91CL/91CL-VPN/$(urlencode '美区AppleID免费共享.md')"
+url_html=$(curl -fsSL $url)
+echo $url_html | grep -Po "免费分享账号：.+?密码：[^ ]+ " | awk -F '[： ]' '{print $2"\t"$4}'
