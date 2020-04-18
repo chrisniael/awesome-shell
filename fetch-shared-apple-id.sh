11bb8d2b63048ef4552cf350c2aa0ddfa088e55e
shenyu <shenyu@shenyu.me>
2020-04-18 23:51:19 +0800

    Update fetch-shared-apple-id.sh url
    Decode CloudFlare email obfuscation

--- fetch-shared-apple-id.sh
+++ fetch-shared-apple-id.sh
@@ -13,8 +13,46 @@ urlencode() {
   return 0
 }
 
-url="https://raw.githubusercontent.com/v2net/Apple/master/README.md"
-curl -fsSL $url | grep "免费分享账号" | awk -F '[： ]' '{print $3"\t"$5}'
+hex2dec() {
+  local hex=$1
+  local dec=$[16#$hex]
+  echo $dec
+}
+
+dec2ch() {
+  local dec=$1
+  local ch=$(printf "\x$(printf %x $dec)")
+  echo $ch
+}
+
+
+# Cloudflare 邮箱混淆技术
+# https://blog.shiniv.com/2016/09/decode-encode-cloudflare-address-obfuscation/
+# https://support.cloudflare.com/hc/en-us/articles/200170016-What-is-Email-Address-Obfuscation-
+decode_email() {
+  local cfcode=$1
+  # echo $cfcode
+  local key=$(hex2dec ${cfcode:0:2})
+  # echo $key
+  local real_email=
+  for ((i=2; i < ${#cfcode}; i=i+2))
+  do
+    hex=${cfcode:$i:2}
+    dec=$(hex2dec $hex)
+    ch=$(dec2ch $(($dec ^ $key)))
+    # echo $i $hex $dec $ch
+    real_email="${real_email}${ch}"
+  done
+  echo -n $real_email
+}
+
+
+url="https://appledi.com"
+encode_data=$(curl -s https://appledi.com | grep 免费分享账号 | grep -Eo "data-cfemail=\".+?\"" | awk -F '"' '{print $2}')
+# echo $encode_data
+real_email=$(decode_email $encode_data)
+passwd=$(curl -s https://appledi.com | grep 免费分享账号 | grep -Eo "密码：.+? " | awk -F '：' '{print $2}')
+printf "${real_email}\t${passwd}\n"
 
 # https://raw.githubusercontent.com/wiki/shadowrocketHelp/help/国外-appstore-id-账号分享.md"
 url="https://raw.githubusercontent.com/wiki/shadowrocketHelp/help/$(urlencode '国外-appstore-id-账号分享.md')"
